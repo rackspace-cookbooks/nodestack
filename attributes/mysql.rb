f5b067ea62f70c5a399833fb9cf8bab452097cdf
Marco A. Morales <marcoamorales@gmail.com>
2014-06-23 14:59:23 -0500

    merging local branch

---
+++ attributes/mysql.rb
@@ -0,0 +1,8 @@
+
+default['mysql-multi']['master'] = ''
+default['mysql-multi']['slaves'] = []
+default['mysql-multi']['slave_user'] = 'replicant'
+
+default['nodestack']['app_db_name'] = 'exampledb'
+default['nodestack']['app_user'] = 'foobar'
+default['nodestack']['app_password'] = nil
---
+++ recipes/mysql_base.rb
@@ -0,0 +1,73 @@
+#
+# Cookbook Name:: nodestack
+# Recipe:: mysql_base
+#
+# Copyright 2014, Rackspace Hosting
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# run apt-get update to clear cache issues
+include_recipe 'apt' if node.platform_family?('debian')
+
+include_recipe 'chef-sugar'
+include_recipe 'database::mysql'
+include_recipe 'platformstack::monitors'
+::Chef::Recipe.send(:include, Opscode::OpenSSL::Password)
+
+include_recipe 'mysql::server'
+
+include_recipe 'mysql-multi::mysql_base'
+
+connection_info = {
+  host: 'localhost',
+  username: 'root',
+  password: node['mysql']['server_root_password']
+}
+
+# add holland user (if holland is enabled)
+if node.deep_fetch('holland', 'enabled')
+  mysql_database_user 'holland' do
+    connection connection_info
+    password ['holland']['password']
+    host 'localhost'
+    privileges [:usage, :select, :'lock tables', :'show view', :reload, :super, :'replication client']
+    retries 2
+    retry_delay 2
+    action [:create, :grant]
+  end
+end
+
+node.set_unless['nodestack']['cloud_monitoring']['agent_mysql']['password'] = secure_password
+
+mysql_database_user node['nodestack']['cloud_monitoring']['agent_mysql']['user'] do
+  connection connection_info
+  password node['nodestack']['cloud_monitoring']['agent_mysql']['password']
+  action 'create'
+end
+
+if node['platformstack']['cloud_monitoring']['enabled'] == true
+  template 'mysql-monitor' do
+    cookbook 'nodestack'
+    source 'monitoring-agent-mysql.yaml.erb'
+    path '/etc/rackspace-monitoring-agent.conf.d/agent-mysql-monitor.yaml'
+    owner 'root'
+    group 'root'
+    mode '00600'
+    notifies 'restart', 'service[rackspace-monitoring-agent]', 'delayed'
+    action 'create'
+  end
+end
+
+#allow the app nodes to connect
+search_add_iptables_rules('recipes:nodestack\:\:application_php' << " AND chef_environment:#{node.chef_environment}", 'INPUT', '-p tcp --dport 3306 -j ACCEPT', 9998, 'allow app nodes to connect')
