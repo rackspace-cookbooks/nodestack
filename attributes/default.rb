b005aaf34daafdb7c462a16269894cf1d8220c58
Marco A. Morales <marcoamorales@gmail.com>
2014-06-25 11:00:02 -0500

    replacing double quotes for single quotes :)

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,20 +1,20 @@
 # attributes/default.rb
 
-default["nodestack"]["appName"] = "my_nodejs_app"
-default["nodestack"]["server_name"] = "defaut_server.js"
-default["nodestack"]["destination"] = "/var/app"
+default['nodestack']['appName'] = 'my_nodejs_app'
+default['nodestack']['server_name'] = 'defaut_server.js'
+default['nodestack']['destination'] = '/var/app'
 default['nodestack']['app_user'] = 'nodejs'
-default["nodestack"]["git_repo"] = 'git@github.com:jrperritt/nodeTestApp.git'
-default["nodestack"]["rev"] = "HEAD"
-default["nodestack"]["deploy_key"] = nil
-default["nodestack"]["domain"] = "localhost"
-default["nodestack"]["http_port"] = "80"
-default["nodestack"]["https_port"] = "443"
-default["nodestack"]["sslcert"] = nil
-default["nodestack"]["sslkey"] = nil
-default["nodestack"]["sslcacert"] = nil
+default['nodestack']['git_repo'] = 'git@github.com:jrperritt/nodeTestApp.git'
+default['nodestack']['rev'] = 'HEAD'
+default['nodestack']['deploy_key'] = nil
+default['nodestack']['domain'] = 'localhost'
+default['nodestack']['http_port'] = '80'
+default['nodestack']['https_port'] = '443'
+default['nodestack']['sslcert'] = nil
+default['nodestack']['sslkey'] = nil
+default['nodestack']['sslcacert'] = nil
 #A comma separated string of packages
-default["nodestack"]["packages"] = ""
+default['nodestack']['packages'] = ''
 
-set['authorization']['sudo']['users'] = ["#{node['nodestack']['username']}"]
+set['authorization']['sudo']['users'] = ['#{node['nodestack']['app_user']}']
 node.set['authorization']['sudo']['passwordless'] = true
