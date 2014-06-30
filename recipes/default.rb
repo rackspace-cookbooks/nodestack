b1564ca783107ffec2464e5282330fabf5259f09
Martin Smith <martin@mbs3.org>
2014-06-30 16:19:12 -0500

    s/listening_port/http_port/g and add appropriate loop for multiple apps

--- recipes/default.rb
+++ recipes/default.rb
@@ -26,5 +26,8 @@ end
 include_recipe 'nodestack::application_nodejs'
 
 include_recipe 'platformstack::iptables'
-add_iptables_rule('INPUT', "-m tcp -p tcp --dport #{node['nodestack']['listening_port']} -j ACCEPT", 100, 'Allow nodejs http traffic')
-add_iptables_rule('INPUT', "-m tcp -p tcp --dport #{node['nodestack']['https_port']} -j ACCEPT", 100, 'Allow nodejs https traffic')
+
+node['nodestack']['apps'].each_pair do |app_name, app_config| # each app loop
+  add_iptables_rule('INPUT', "-m tcp -p tcp --dport #{app_config['http_port']} -j ACCEPT", 100, 'Allow nodejs http traffic')
+  add_iptables_rule('INPUT', "-m tcp -p tcp --dport #{app_config['nodestack']['https_port']} -j ACCEPT", 100, 'Allow nodejs https traffic')
+end
