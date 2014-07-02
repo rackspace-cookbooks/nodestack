3814c38ea6a9b1338a50db2d540947845c2ac7c9
Marco A. Morales <marcoamorales@gmail.com>
2014-07-01 23:29:03 -0500

    removing more stuff that is no longer used

--- attributes/default.rb
+++ attributes/default.rb
@@ -6,7 +6,6 @@ default['nodestack']['apps']['my_nodejs_app']['git_repo'] = 'https://github.com/
 default['nodestack']['apps']['my_nodejs_app']['entry_point'] = 'server.js'
 default['nodestack']['apps']['my_nodejs_app']['rev'] = 'HEAD'
 default['nodestack']['apps']['my_nodejs_app']['deploy_key'] = nil
-default['nodestack']['apps']['my_nodejs_app']['domain'] = 'localhost'
 default['nodestack']['apps']['my_nodejs_app']['http_port'] = '8080'
 default['nodestack']['apps']['my_nodejs_app']['https_port'] = '443'
 default['nodestack']['apps']['my_nodejs_app']['mysql_app_user_password'] = 'randompass'
--- attributes/mysql.rb
+++ attributes/mysql.rb
@@ -2,7 +2,3 @@
 default['mysql-multi']['master'] = ''
 default['mysql-multi']['slaves'] = []
 default['mysql-multi']['slave_user'] = 'replicant'
-
-default['nodestack']['app_db_name'] = 'test'
-default['nodestack']['app_db_user'] = 'testuser'
-default['nodestack']['app_db_user_password'] = nil
