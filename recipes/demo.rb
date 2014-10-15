05b31690a6fb27d0cead6170ae9b315afa306d4d
Marco A. Morales <marcoamorales@gmail.com>
2014-10-15 14:41:11 -0500

    fixed swp gitignore

---
+++ recipes/demo.rb
@@ -0,0 +1,15 @@
+node.set['nodestack']['apps']['my_nodejs_app']['app_dir'] = '/var/app'
+node.set['nodestack']['apps']['my_nodejs_app']['app_options'] = []
+node.set['nodestack']['apps']['my_nodejs_app']['git_repo'] = 'git@github.com:marcoamorales/node-hello-world.git'
+node.set['nodestack']['apps']['my_nodejs_app']['git_rev'] = 'HEAD'
+node.set['nodestack']['apps']['my_nodejs_app']['git_repo_domain'] = 'github.com'
+node.set['nodestack']['apps']['my_nodejs_app']['enable_submodules'] = false
+node.set['nodestack']['apps']['my_nodejs_app']['entry_point'] = 'app.js'
+node.set['nodestack']['apps']['my_nodejs_app']['npm'] = true
+node.set['nodestack']['apps']['my_nodejs_app']['config_file'] = true
+node.set['nodestack']['apps']['my_nodejs_app']['env']['PORT'] = '80'
+node.set['nodestack']['apps']['my_nodejs_app']['env']['MONGO_PORT'] = '27017'
+node.set['nodestack']['apps']['my_nodejs_app']['monitoring']['body'] = 'Hello World!'
+node.set['nodestack']['apps']['my_nodejs_app']['npm_options'] = ['--production']
+node.set['nodestack']['apps']['my_nodejs_app']['deployment']['before_symlink'] = nil
+node.set['nodestack']['cookbook'] = 'nodestack'
